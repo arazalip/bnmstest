65f1d698e72cae097c97d7892b89ecad8e1c9e50
amininama <foozoolak@gmail.com>
2012-06-21 19:30:56 +0430

    revised compareTo method

--- src/main/java/com/bourse/nms/entity/Order.java
+++ src/main/java/com/bourse/nms/entity/Order.java
@@ -47,31 +47,18 @@ public class Order implements Comparable {
     @Override
     public int compareTo(Object o) {
         final Order order = (Order) o;
-        return (int) (this.orderCode - order.getOrderCode());
+        //todo: find a faster way... note that casting from long to int can generate pure nonsense
+        final long dif = (this.orderCode - order.getOrderCode());
+        return dif == 0 ? 0 : (int) (dif / Math.abs(dif));
     }
 
-    private long buildOrderCode(long price, int priority, long time) {
-        return (price & 0x7FFFFl) << 44 | ((priority & 0x0Fl) << 40) | (time & 0xFFFFFFFFFFl);
+    public static long buildOrderCode(long price, int priority, long time) {
+        return (price & 0x7FFFFl) << 44 | ((priority & 0x0Fl) << 40) | ((time & 0xFFFFFFFFFF000l) >> 12);
     }
 
     public static void main(String[] args) {
-        final long time = Long.MAX_VALUE - System.nanoTime();
-        final byte priority = (byte) 5;
-        final int price = 100000;
-        System.out.println(time & 0xFFFFFFFFFFl);
-        System.out.println(priority & 0x0F);
-        System.out.println(price & 0x7FFFF);
-
-        final long orderCode = ((price & 0x7FFFFl) << 44) | ((priority & 0x0Fl) << 40) | (time & 0xFFFFFFFFFFl);
-        System.out.println(orderCode);
-
-        final long fetchedPrice = (orderCode & 0xFFFFF00000000000l) >> 44;
-        System.out.println(fetchedPrice);
-
-        final byte fetchedPriority = (byte) ((orderCode & 0x00000F0000000000l) >> 40);
-        System.out.println(fetchedPriority);
-
-        final long fetchedTime = (orderCode & 0xFFFFFFFFFFl);
-        System.out.print(fetchedTime);
+        final Order order1 = new Order(10, (byte) 10, 10000, 10);
+        final Order order2 = new Order(10, (byte) 10, 10000, 10);
+        System.out.println(order1.compareTo(order2));
     }
 }
